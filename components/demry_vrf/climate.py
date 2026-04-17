d339f3cb51e1d1c55259c803abbbb7041e509b82
diy97 <44125982+diy97@users.noreply.github.com>
2026-04-17 09:02:44 +0800

    Update climate.py

--- components/demry_vrf/climate.py
+++ components/demry_vrf/climate.py
@@ -4,12 +4,13 @@ from esphome.components import climate
 from esphome.const import CONF_ID
 from . import DemryVrfComponent, DEMRY_VRF_ID
 
+
 DEPENDENCIES = ['demry_vrf']
 
 demry_vrf_ns = cg.esphome_ns.namespace('demry_vrf')
 DemryVrfClimate = demry_vrf_ns.class_('DemryVrfClimate', cg.Component, climate.Climate)
 
-CONFIG_SCHEMA = climate.PLATFORM_SCHEMA.extend({
+CONFIG_SCHEMA = climate.CLIMATE_PLATFORM_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(DemryVrfClimate),
     cv.GenerateID(DEMRY_VRF_ID): cv.use_id(DemryVrfComponent),
     cv.Required("idx"): cv.int_range(min=0, max=31),
